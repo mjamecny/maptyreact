bb92d279c39a2d73b118fbdfdfd824df0d47f908
mjamecny <jamecny.m@tutanota.com>
2023-10-05 14:24:56 +0200

    create appSlice

---
+++ src/features/app/appSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {}
+
+const appSlice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {},
+})
+
+export default appSlice.reducer
