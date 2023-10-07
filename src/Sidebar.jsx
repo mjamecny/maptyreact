5e3069d94ba2b63a9f00a5ff84d657e833b79e5c
mjamecny <jamecny.m@tutanota.com>
2023-10-07 15:52:02 +0200

    add logic for show exercises

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,61 +1,92 @@
+import { useDispatch, useSelector } from "react-redux"
+
 import Copyright from "./Copyright"
 import Form from "./Form"
 
+import { getExercises, setExCoords } from "./features/app/exerciseSlice"
+
 export default function Sidebar() {
+  const exercises = useSelector(getExercises)
+  const dispatch = useDispatch()
+
+  function handleChangeCenter(coords) {
+    dispatch(setExCoords(coords))
+  }
+
   return (
     <div className="sidebar">
       <img src="../logo.png" alt="Logo" className="logo" />
 
       <ul className="workouts">
         <Form />
+        {exercises.map((exercise) => {
+          const { id, date, type, distance, duration, coords } = exercise
+          const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ]
+          const newDate = new Date(date)
+          const day = newDate.getDate()
+          const month = months[newDate.getMonth()]
 
-        <li className="workout workout--running">
-          <h2 className="workout__title">Running on April 14</h2>
-          <div className="workout__details">
-            <span className="workout__icon">🏃‍♂️</span>
-            <span className="workout__value">5.2</span>
-            <span className="workout__unit">km</span>
-          </div>
-          <div className="workout__details">
-            <span className="workout__icon">⏱</span>
-            <span className="workout__value">24</span>
-            <span className="workout__unit">min</span>
-          </div>
-          <div className="workout__details">
-            <span className="workout__icon">⚡️</span>
-            <span className="workout__value">4.6</span>
-            <span className="workout__unit">min/km</span>
-          </div>
-          <div className="workout__details">
-            <span className="workout__icon">🦶🏼</span>
-            <span className="workout__value">178</span>
-            <span className="workout__unit">spm</span>
-          </div>
-        </li>
-
-        <li className="workout workout--cycling">
-          <h2 className="workout__title">Cycling on April 5</h2>
-          <div className="workout__details">
-            <span className="workout__icon">🚴‍♀️</span>
-            <span className="workout__value">27</span>
-            <span className="workout__unit">km</span>
-          </div>
-          <div className="workout__details">
-            <span className="workout__icon">⏱</span>
-            <span className="workout__value">95</span>
-            <span className="workout__unit">min</span>
-          </div>
-          <div className="workout__details">
-            <span className="workout__icon">⚡️</span>
-            <span className="workout__value">16</span>
-            <span className="workout__unit">km/h</span>
-          </div>
-          <div className="workout__details">
-            <span className="workout__icon">⛰</span>
-            <span className="workout__value">223</span>
-            <span className="workout__unit">m</span>
-          </div>
-        </li>
+          return (
+            <li
+              key={id}
+              className={`workout ${
+                type === "running" ? "workout--running" : "workout--cycling"
+              }`}
+              onClick={() => handleChangeCenter(coords)}
+            >
+              <h2 className="workout__title">
+                {type === "running"
+                  ? `Running on ${month} ${day}`
+                  : `Cycling on ${month} ${day}`}
+              </h2>
+              <div className="workout__details">
+                <span className="workout__icon">
+                  {type === "running" ? "🏃‍♂️" : "🚴‍♀️"}
+                </span>
+                <span className="workout__value">{distance}</span>
+                <span className="workout__unit">km</span>
+              </div>
+              <div className="workout__details">
+                <span className="workout__icon">⏱</span>
+                <span className="workout__value">{duration}</span>
+                <span className="workout__unit">min</span>
+              </div>
+              <div className="workout__details">
+                <span className="workout__icon">⚡️</span>
+                <span className="workout__value">
+                  {type === "running" ? exercise.pace : exercise.speed}
+                </span>
+                <span className="workout__unit">
+                  {type === "running" ? "min/km" : "km/h"}
+                </span>
+              </div>
+              <div className="workout__details">
+                <span className="workout__icon">
+                  {type === "running" ? "🦶🏼" : "⛰"}
+                </span>
+                <span className="workout__value">
+                  {type === "running" ? exercise.cadence : exercise.elevation}
+                </span>
+                <span className="workout__unit">
+                  {type === "running" ? "spm" : "m"}
+                </span>
+              </div>
+            </li>
+          )
+        })}
       </ul>
 
       <Copyright />
