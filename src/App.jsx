8ab6c02e849919fa44f2554c4c0daa57eb87a081
mjamecny <jamecny.m@tutanota.com>
2023-10-07 16:38:23 +0200

    import react-hot-toast to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
 
 import AppLayout from "./AppLayout"
 import Form from "./Form"
@@ -15,13 +16,31 @@ export default function App() {
   }, [dispatch])
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="app" element={<AppLayout />}>
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route index element={<Navigate replace to="app" />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="app" element={<AppLayout />}>
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route index element={<Navigate replace to="app" />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "1.4rem",
+            fontFamily: "Manrope, sans-serif",
+            color: "var(--color-light--2)",
+            backgroundColor: "var(--color-dark--1)",
+          },
+        }}
+      />
+    </>
   )
 }
